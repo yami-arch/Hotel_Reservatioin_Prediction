f8d50cdb85ad99ddbfab43e126904a53561605bc
Yami6575 <nikhil.mishra112004@gmail.com>
2025-05-26 22:08:10 +0530

    entire pipeline donw

---
+++ src/custom_exception.py
@@ -0,0 +1,21 @@
+import traceback
+import sys
+
+class CustomException(Exception):
+
+    def __init__(self, error_message, error_detail):
+        super().__init__(error_message)
+        self.error_message = self.get_detailed_error_message(error_message, error_detail)
+
+    @staticmethod
+    def get_detailed_error_message(error_message, error_detail):
+        try:
+            _, _, exc_tb = sys.exc_info()
+            file_name = exc_tb.tb_frame.f_code.co_filename
+            line_number = exc_tb.tb_lineno
+            return f"Error in {file_name}, line {line_number}: {error_message}"
+        except:
+            return f"Error: {error_message}"
+    
+    def __str__(self):
+        return self.error_message
